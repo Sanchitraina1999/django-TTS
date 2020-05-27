2d68afeb3117791fe9731106289a0a1c813c2d3f
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-27 20:04:44 +0530

    --- Auto-Git Commit ---

--- TTS/TTS/views.py
+++ TTS/TTS/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os 
+from gtts import gTTS 
+from playsound import playsound
 
 def home(request):
     return render(request,'index.html')
+
+def speech(request):
+    mytext = request.POST.get('text','')
+    print(mytext)
+    language = 'en'
+    tts = gTTS(text=mytext, lang=language, slow=False) 
+    tts.save("welcome.mp3") 
+    playsound('welcome.mp3')
+    return render(request,'speech.html')
